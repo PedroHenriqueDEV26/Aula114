19ec7f1ee4ec1a8b9af55ee98bdce09668a7af55
PedroHenriqueDEV26 <112581681+PedroHenriqueDEV26@users.noreply.github.com>
2024-02-06 14:53:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var video = ""
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(500,400);
+    canvas.center();
+    video = createCapture(VIDEO)
+    video.hide()
+    
+}
+function draw(){
+    image(video,0, 0, 500, 400)
+}
+
+function takeSnapshot(){
+    save("img.png")
+}
+
