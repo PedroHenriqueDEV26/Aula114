f308490674a318525494b6d5b9356957a1dc187b
PedroHenriqueDEV26 <112581681+PedroHenriqueDEV26@users.noreply.github.com>
2024-02-27 14:48:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var video = ""
+var results = []
+var narizX, narizY
 function preload(){
 
 }
@@ -8,10 +10,30 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO)
     video.hide()
+
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on("pose", gotPoses)
+}
+
+function modelLoaded(){
+    console.log("modelLoaded")
+}
+
+function gotPoses(results){
+   if(results.length > 0){
+    // console.log(results);
+    // console.log("narizX"+results[0].pose.nose.x)
+    // console.log("narizY"+results[0].pose.nose.y)
+    narizX = results[0].pose.nose.x - 96;
+    narizY = results[0].pose.nose.y - 50;
+   }
     
 }
+
 function draw(){
     image(video,0, 0, 500, 400)
+    fill("red")
+    circle(narizX, narizY, 25)
 }
 
 function takeSnapshot(){
